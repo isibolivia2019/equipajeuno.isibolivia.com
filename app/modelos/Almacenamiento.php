89a686c0b156b9fe982618100e2d19f6aa9ec60e
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-19 11:49:21 -0400

    subida nro. 1

--- app/modelos/Almacenamiento.php
+++ app/modelos/Almacenamiento.php
@@ -12,7 +12,7 @@ class Almacenamiento{
 	}
 
 	public function listaTarjetaOcupada($datos){
-		$sql = "SELECT cod_cliente, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, usuario_inicio, detalle_final, fecha_final hora_final, usuario_final FROM almacenamiento, cliente WHERE almacenamiento.cod_cliente = cliente.cod_cliente and fecha_final = null";
+		$sql = "SELECT almacenamiento.codigo, cod_cliente, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, usuario_inicio FROM almacenamiento, cliente WHERE almacenamiento.cod_cliente = cliente.codigo and fecha_final IS NULL;";
 		return $this->db->select($sql, $datos);
 	}
 }
