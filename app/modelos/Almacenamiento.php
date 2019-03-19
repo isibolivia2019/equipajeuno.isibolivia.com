f759c9fbc30cdce05be4aa50b77169b7770ee3e6
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-19 15:22:12 -0400

    subida nro. 1

--- app/modelos/Almacenamiento.php
+++ app/modelos/Almacenamiento.php
@@ -17,7 +17,7 @@ class Almacenamiento{
 	}
 
 	public function almacenamientoEntregaEspecifico($datos){
-		$sql = "SELECT almacenamiento.codigo, CONCAT(cliente.nombre, ' ', cliente.appat, ' ', cliente.apmat) as 'cliente', cliente.ci, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, CONCAT(usuario.nombre, ' ', usuario.appat, ' ', usuario.apmat) as 'personal' FROM almacenamiento, cliente, usuario WHERE almacenamiento.cod_cliente = cliente.codigo and almacenamiento.usuario_inicio = usuario.cod_usuario and cod_tarjeta = ? and fecha_final IS NULL;";
+		$sql = "SELECT almacenamiento.codigo, CONCAT(cliente.nombre, ' ', cliente.appat, ' ', cliente.apmat) as 'cliente', cliente.ci, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, CONCAT(usuario.nombre, ' ', usuario.appat, ' ', usuario.apmat) as 'personal', DATEDIFF(CURDATE(), fecha_inicio) as 'dias' FROM almacenamiento, cliente, usuario WHERE almacenamiento.cod_cliente = cliente.codigo and almacenamiento.usuario_inicio = usuario.cod_usuario and cod_tarjeta = ? and fecha_final IS NULL;";
 		return $this->db->select($sql, $datos);
 	}
 }
