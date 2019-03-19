20961b4998bb4ac57ac9f5711467841d8d72ee88
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-18 22:11:25 -0400

    subida nro. 1

--- app/modelos/Gasto.php
+++ app/modelos/Gasto.php
@@ -12,7 +12,7 @@ class Gasto{
 	}
 
 	public function listaGasto($datos){
-		$sql = "SELECT monto, detalle, comprobante, gastos.cod_usuario, fecha, hora, CONCAT(nombre, ' ', appat, ' ', apmat) as 'personal' FROM gastos, usuario WHERE gastos.cod_usuario = usuario.cod_usuario;";
+		$sql = "SELECT codigo, monto, detalle, comprobante, gastos.cod_usuario, fecha, hora, CONCAT(nombre, ' ', appat, ' ', apmat) as 'personal' FROM gastos, usuario WHERE gastos.cod_usuario = usuario.cod_usuario;";
 		return $this->db->select($sql, $datos);
 	}
     
