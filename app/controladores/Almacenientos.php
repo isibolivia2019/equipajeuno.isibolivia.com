39bb4090b9ce9771210454f77f72333c5f5b64d4
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-19 09:05:34 -0400

    subida nro. 1

---
+++ app/controladores/Almacenientos.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', '1');
+session_start();
+require_once 'RegistrosNotificaciones.php';
+$action = '';
+
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+    switch ($action) {
+        case 'agregarEquipaje' :
+            agregarEquipaje();
+            break;
+    }
+}
+
+function modelo($modelo){
+    require_once '../modelos/'.$modelo.'.php';
+    return new $modelo();
+}
+
+function agregarEquipaje(){
+    $codigoSession = $_POST['codigoSession'];
+    $codigoTarjeta = $_POST['codigoTarjeta'];
+    $codigoCliente = $_POST['codigoCliente'];
+    $pago = $_POST['pago'];
+    $costo = $_POST['costo'];
+    $detalle = $_POST['detalle'];
+
+    $nombreImagen = $_POST['nombreImagen'];
+    $imagen = $_POST['imagen'];
+    if($nombreImagen != "sin_imagen_equipaje.jpg"){
+        file_put_contents("../../public/imagenes/gastos/".$nombreImagen, base64_decode($imagen));
+    }
+    
+    date_default_timezone_set('America/La_Paz');
+    $hora = date("H:i:s");
+    $fecha = date("Y-m-d");
+    $resp = "";
+
+    $datos = array($codigoCliente, $codigoTarjeta, $nombreImagen, $pago, $costo, $costo, $detalle, $fecha, $hora, $codigoSession);
+    $modelo = modelo('Almacenamiento');
+    $resp = $modelo->agregarEquipaje($datos);
+
+    $registrosNotificaciones = new RegistrosNotificaciones();
+    $registrosNotificaciones->agregarRegistro($codigoSession, "Se registro el recepcionamiento de un nuevo equipaje  (".$detalle.") con Monto de: Bs. ".$monto);
+
+    $data = ['resp' => $resp];
+    echo json_encode($data);
+}
+?>
---
+++ app/modelos/Almacenamiento.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Base.php';
+class Gasto{
+	private $db;
+	public function __construct(){
+		$this->db = new Base;
+	}
+
+  	public function agregarEquipaje($datos){
+		$sql = "INSERT INTO almacenamiento(cod_cliente, cod_tarjeta, imagen_uno, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, usuario_inicio) VALUES(?,?,?,?,?,?,?,?,?,?);";
+		return $this->db->insert($sql, $datos);
+	}
+
+
+}
+?>
