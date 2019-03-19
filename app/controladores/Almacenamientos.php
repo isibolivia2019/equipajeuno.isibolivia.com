c195899d8d776cc23136efa07bdf5c1d463af6d4
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-19 16:01:16 -0400

    subida nro. 1

--- app/controladores/Almacenamientos.php
+++ app/controladores/Almacenamientos.php
@@ -19,6 +19,9 @@ if (isset($_POST['action'])) {
         case 'almacenamientoEntregaEspecifico' :
             almacenamientoEntregaEspecifico();
             break;
+        case 'devolverEquipaje' :
+            devolverEquipaje();
+            break;
     }
 }
 
@@ -56,6 +59,34 @@ function agregarEquipaje(){
 
     $registrosNotificaciones = new RegistrosNotificaciones();
     $registrosNotificaciones->agregarRegistro($codigoSession, "Se registro el recepcionamiento de un nuevo equipaje  (".$detalle.") con el Costo de: Bs. ".$costo);
+
+    $data = ['resp' => $resp];
+    echo json_encode($data);
+}
+
+function devolverEquipaje(){
+    $codigoSession = $_POST['codigoSession'];
+    $codigoTarjeta = $_POST['codigoTarjeta'];
+    $codigoCliente = $_POST['codigoCliente'];
+    $totalCosto = $_POST['totalCosto'];
+    $observacion = $_POST['observacion'];
+    $detalleInicio = $_POST['detalleInicio'];
+
+    date_default_timezone_set('America/La_Paz');
+    $hora = date("H:i:s");
+    $fecha = date("Y-m-d");
+    $resp = "";
+
+    $datos = array($totalCosto, $observacion, $fecha, $hora, $codigoSession, $codigoTarjeta, $codigoCliente);
+    $modelo = modelo('Almacenamiento');
+    $resp = $modelo->devolverEquipaje($datos);
+
+    $datos = array("0", $codigoTarjeta);
+    $modelo = modelo('Tarjeta');
+    $resp = $modelo->cambiarEstado($datos);
+
+    $registrosNotificaciones = new RegistrosNotificaciones();
+    $registrosNotificaciones->agregarRegistro($codigoSession, "Se registro ela entrega del equipaje  (".$detalleInicio.") con un Costo Total de: Bs. ".$totalCosto);
 
     $data = ['resp' => $resp];
     echo json_encode($data);
--- app/modelos/Almacenamiento.php
+++ app/modelos/Almacenamiento.php
@@ -20,5 +20,10 @@ class Almacenamiento{
 		$sql = "SELECT almacenamiento.codigo, CONCAT(cliente.codigo) as 'cod_cliente', CONCAT(cliente.nombre, ' ', cliente.appat, ' ', cliente.apmat) as 'cliente', cliente.ci, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, CONCAT(usuario.nombre, ' ', usuario.appat, ' ', usuario.apmat) as 'personal', DATEDIFF(CURDATE(), fecha_inicio) as 'dias' FROM almacenamiento, cliente, usuario WHERE almacenamiento.cod_cliente = cliente.codigo and almacenamiento.usuario_inicio = usuario.cod_usuario and cod_tarjeta = ? and fecha_final IS NULL;";
 		return $this->db->select($sql, $datos);
 	}
+
+	public function devolverEquipaje($datos){
+		$sql = "UPDATE almacenamiento SET costo_total = ?, detalle_final = ?, fecha_final = ?, hora_final = ?, usuario_final = ? WHERE cod_tarjeta = ? and cod_cliente = ?";
+		return $this->db->update($sql, $datos);
+	}
 }
 ?>
