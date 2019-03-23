e3363413c78a6037891f4c19a297035eec86e3db
isibolivia2019 <isibolivia2019@gmail.com>
2019-03-23 12:07:52 -0400

    subida nro. 1

--- app/modelos/Almacenamiento.php
+++ app/modelos/Almacenamiento.php
@@ -17,7 +17,7 @@ class Almacenamiento{
 	}
 
 	public function HistorialClienteEquipaje($datos){
-		$sql = "SELECT almacenamiento.codigo, cod_cliente, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, usuario_inicio, detalle_final, fecha_final, hora_final, usuario_final FROM almacenamiento, cliente WHERE almacenamiento.cod_cliente = cliente.codigo and almacenamiento.cod_cliente = ?;";
+		$sql = "SELECT almacenamiento.codigo, cod_cliente, cod_tarjeta, imagen_uno, imagen_dos, imagen_tres, pago_anticipado, costo, costo_total, detalle_inicio, fecha_inicio, hora_inicio, usuario_inicio, detalle_final, fecha_final, hora_final, usuario_final, DATEDIFF(DATE_SUB(now(), INTERVAL 4 HOUR), fecha_inicio) as 'dias' FROM almacenamiento, cliente WHERE almacenamiento.cod_cliente = cliente.codigo and almacenamiento.cod_cliente = ?;";
 		return $this->db->select($sql, $datos);
 	}
 
